73be55d975e278a9115098b16071a182570fe84c
Duda <mariaeduardaciarini03@gmail.com>
2025-05-27 11:41:00 -0300

    Adicionando projeto inicial

---
+++ cursojavamongo/src/main/java/com/webservicenosql/mongo/CursojavamongoApplication.java
@@ -0,0 +1,13 @@
+package com.webservicenosql.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavamongoApplication.class, args);
+	}
+
+}
---
+++ cursojavamongo/src/test/java/com/webservicenosql/mongo/CursojavamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicenosql.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
