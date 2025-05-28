2aff326da106d76d19fbc40a3fd631469d983108
Duda <mariaeduardaciarini03@gmail.com>
2025-05-28 13:42:55 -0300

    Consulta simples com query methods

--- cursojavamongo/src/main/java/com/webservicenosql/mongo/resources/PostResource.java
+++ cursojavamongo/src/main/java/com/webservicenosql/mongo/resources/PostResource.java
@@ -1,25 +1,38 @@
 package com.webservicenosql.mongo.resources;
 
+import java.util.List;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webservicenosql.mongo.domain.Post;
 import com.webservicenosql.mongo.service.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
 
 	@GetMapping("/{id}")
- 	public ResponseEntity<Post> findById(@PathVariable String id) {
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URLDecoder.decode(text, StandardCharsets.UTF_8);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
