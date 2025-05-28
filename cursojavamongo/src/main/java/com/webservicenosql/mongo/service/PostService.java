3d5eef96e8f782c45be8ee51921be7de83bb5aad
Duda <mariaeduardaciarini03@gmail.com>
2025-05-28 04:57:23 -0300

    Obtendo um post por id
    No postman não vem só a exception [200~{
        "timestamp": 1748418851547,
        "status": 404,
        "error": "Não encontrado",
        "message": "Objeto não encontrado",
        "path": "/posts/6836bdfee5b02eed83ecc1ec"
    }~

---
+++ cursojavamongo/src/main/java/com/webservicenosql/mongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.webservicenosql.mongo.service;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservicenosql.mongo.domain.Post;
+import com.webservicenosql.mongo.repository.PostRepository;
+import com.webservicenosql.mongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
