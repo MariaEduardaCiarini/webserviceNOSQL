42c956f09cf2b4af30f5547c9bd48b7fd51b31f8
Duda <mariaeduardaciarini03@gmail.com>
2025-05-28 04:44:09 -0300

    Endpoint para retornar os posts de um usuario

--- cursojavamongo/src/main/java/com/webservicenosql/mongo/resources/UserResource.java
+++ cursojavamongo/src/main/java/com/webservicenosql/mongo/resources/UserResource.java
@@ -1,14 +1,22 @@
 package com.webservicenosql.mongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import com.webservicenosql.mongo.domain.Post;
 import com.webservicenosql.mongo.domain.User;
 import com.webservicenosql.mongo.dto.UserDTO;
 import com.webservicenosql.mongo.service.UserService;
@@ -67,5 +75,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
